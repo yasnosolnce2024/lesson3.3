fcc1f025c221f5b57d50e6f22e779ee78cf6e480
yasnosolnce2024 <pomogaika51@gmail.com>
2024-03-24 07:21:07 +0700

    add new count

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра Тир')
+
+icon = pygame.image.load('img / 42648.jpg')
+
 runnig = True
 while runnig:
     pass
