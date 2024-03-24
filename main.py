ca42eebd6ff9c9d6ae4cfd69fcb22705912a308f
yasnosolnce2024 <pomogaika51@gmail.com>
2024-03-24 08:36:49 +0700

    add points

--- main.py
+++ main.py
@@ -18,16 +18,26 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+screen.fill(color)
+
+points = 0
+account = pygame.font.SysFont('arial', 36)
+#account_text = account.render(str(points), 1, (0, 0, 0))
+#screen.blit(account_text , (50, 50))
+#pygame.display.update()
 
 running = True
 while running:
     screen.fill(color)
+    account_text = account.render('Попаданий: ' + str(points), 1, (0, 0, 0))
+    screen.blit(account_text, (20, 20))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                points = points + 1
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_img, (target_x, target_y))
